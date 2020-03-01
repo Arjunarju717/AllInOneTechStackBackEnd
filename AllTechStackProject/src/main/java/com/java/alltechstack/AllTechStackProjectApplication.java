cf079c404c3ddc61f7aecf3208324703e012c1c0
an <an@ATH051685.corp.athenahealth.com>
2020-03-01 15:18:46 +0530

    First Commit

---
+++ AllTechStackProject/src/main/java/com/java/alltechstack/AllTechStackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.java.alltechstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllTechStackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllTechStackProjectApplication.class, args);
+	}
+
+}
---
+++ AllTechStackProject/src/test/java/com/java/alltechstack/AllTechStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.alltechstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllTechStackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
